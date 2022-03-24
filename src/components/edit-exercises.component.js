d586352ffe7bace77fda0fd6df74c5aed4d1ca3b
ck-chng <chukaichng@gmail.com>
2022-03-24 23:57:00 +0000

    edit exercise api link

--- src/components/edit-exercises.component.js
+++ src/components/edit-exercises.component.js
@@ -15,7 +15,7 @@ function EditExercises() {
     const { logon, workoutid } = useParams();
 
     useEffect(() => {
-        axios.get('http://localhost:5000/exercises/'+ logon + "/" + workoutid)
+        axios.get('https://fitness-app-ck.netlify.app/exercises/'+ logon + "/" + workoutid)
             .then(response => {
                 setUsername(logon);
                 if (response.data.length > 0) {
